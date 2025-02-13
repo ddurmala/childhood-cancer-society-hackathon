cb466cf161b45afad78df1801a29c6274882dee9
ddurmala <ddurmala@gmail.com>
2025-02-12 23:52:28 -0500

    trying to deploy

--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const connectionString =
     process.env.NODE_ENV === "production"
-        ? process.env.RENDER_DATABASE_URL  // Use the Render database in production
+        ? process.env.DATABASE_URL  // Use the Render database in production
         : process.env.LOCAL_DATABASE_URL; // Use local database when running locally
 
 const pool = new Pool({
